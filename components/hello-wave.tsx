f873245a26ff35ab40d6f9cb406c3b189ae5db15
PalDPathak404 <pdpathak2005@gmail.com>
2026-07-20 15:17:30 +0530

    feat: add hello-wave component for dashboard greetings

---
+++ components/hello-wave.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+export function HelloWave() {
+  return (
+    <Text style={{ fontSize: 28, lineHeight: 32, marginTop: -6 }}>
+      Hello
+    </Text>
+  );
+}
